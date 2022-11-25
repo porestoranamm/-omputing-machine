4b85ca8fe1ff12690bda00580277647a0280ae5a
Nikita Kuznetsov <livisback@yandex.ru>
2022-11-25 14:41:47 +0600

    add function  forEach in buttons

--- JS Calculator/app.js
+++ JS Calculator/app.js
@@ -3,4 +3,14 @@
     let buttons = document.querySelectorAll('.btn');
     let clear  = document.querySelector('.btn-clear');
     let equal = document.querySelector('.btn-equal');
+
+    buttons.forEach(function(button){
+        button.addEventListener('click', function(e){
+        let value = e.target.dataset.num;
+        screen.value += value;
+        })
+    })
+
+
+    
 })();
