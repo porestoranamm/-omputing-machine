5937490d9d2aa83d26e7446870df3c117b4d379c
Nikita Kuznetsov <livisback@yandex.ru>
2022-11-25 15:05:53 +0600

    creat func clear

--- JS Calculator/app.js
+++ JS Calculator/app.js
@@ -9,12 +9,12 @@
         let value = e.target.dataset.num;
         screen.value += value;
         })
-    })
+    });
 
     equal.addEventListener('click', function(e){
-        if(screen.value === ''){
-            screen.value = "";
-        }else{
+        if(screen.value === '') {
+            screen.value = "Please enter";
+        } else{
             let answer = eval(screen.value);
             screen.value = answer;
         }
